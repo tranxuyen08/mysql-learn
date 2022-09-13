b025042cd80a2bc3ec27130d568c703f4fdb2e7d
Trần Ngọc Xuyên <tranngocxuyen@MacBook-Air-cua-Tran.local>
2022-09-13 21:26:04 +0700

    learn mysql order by

---
+++ Select Table/posts.sql
@@ -0,0 +1,3 @@
+SELECT * FROM posts ORDER BY created_at DESC, id DESC;
+
+SELECT * FROM posts ORDER BY title ASC;
--- Select Table/users.sql
+++ Select Table/users.sql
@@ -1,3 +1,9 @@
 SELECT * FROM users WHERE email = 'xuyentran1998@gmail.com' AND password = MD5('12345678');
 
 SELECT * FROM users WHERE email = 'xuyentran1998@gmail.com' OR email = 'ngocxuyen111@gmail.com' ;
+
+# SELECT * FROM table
+INNER JOIN table2 ON table2.table_id = table2.id
+WHERE column1 = '123' AND (column2 = '123' OR column3 = '123')
+ORDER BY column1 DESC, column2 ASC
+LIMIT 1 OFFSET 0;
